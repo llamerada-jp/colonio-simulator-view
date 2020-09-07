0b0e70c5151d4edeca317142f25e5c02db50a6cd
Yuji Ito <llamerada.jp@gmail.com>
2020-09-08 01:56:07 +0900

    update drawer for sphere
    Signed-off-by: Yuji Ito <llamerada.jp@gmail.com>

--- pkg/model2d/sphere.go
+++ pkg/model2d/sphere.go
@@ -9,30 +9,68 @@ import (
 
 type Sphere struct{}
 
+var sphereScale = 0.95
+var sphereColorMap = [][]float32{
+	{0.8, 0.0, 0.8},
+	{0.0, 0.2, 1.0},
+	{0.0, 0.8, 0.2},
+	{1.0, 0.6, 0.0},
+}
+
 func (s *Sphere) draw(gl *utils.GL, nodes map[string]*Node, current *time.Time) error {
 	for _, node := range nodes {
-		gl.SetRGB(0.0, 0.8, 0.2)
-		gl.Point3(s.convertCoordinate(node.x, node.y))
+		if !node.enable {
+			continue
+		}
+
+		colorIdx := node.group
+		if colorIdx >= len(colorMap) {
+			colorIdx = 0
+		}
+		x, y, z := s.convertCoordinate(node.x, node.y)
+		gl.SetRGB(s.reduceColorByZ(colorMap[colorIdx], z))
+		gl.Point3(x, y, z)
+
+		if node.seedLinkStatus == LinkStatusOnline {
+			x, y, z := s.convertCoordinate(node.x, node.y)
+			gl.SetRGB(s.reduceColorByZ([]float32{1.0, 0.0, 0.0}, z))
+			gl.Box3(x, y, z, 6.0)
+		}
+		if node.isOnlyone {
+			x, y, z := s.convertCoordinate(node.x, node.y)
+			gl.SetRGB(s.reduceColorByZ([]float32{1.0, 0.0, 0.0}, z))
+			gl.Box3(x, y, z, 10.0)
+		}
 
 		for _, link := range node.links {
 			if pair, ok := nodes[link]; ok {
+				var rgb []float32
 				if pair.hasLink(node.nid) {
 					if node.hasRequired2D(pair.nid) {
-						gl.SetRGB(0.0, 1.0, 0.2)
+						rgb = colorMap[colorIdx]
 					} else {
-						gl.SetRGB(0.6, 0.6, 0.6)
+						rgb = []float32{0.8, 0.8, 0.8}
 					}
 				} else {
-					gl.SetRGB(0.8, 0.0, 0.0)
+					rgb = []float32{0.8, 0.0, 0.0}
 				}
 				x1, y1, z1 := s.convertCoordinate(node.x, node.y)
 				x2, y2, z2 := s.convertCoordinate(pair.x, pair.y)
+				gl.SetRGB(s.reduceColorByZ(rgb, (z1+z2)/2.0))
 				gl.Line3(x1, y1, z1, x2, y2, z2)
 			}
 		}
 	}
 
 	return nil
+}
+
+func (s *Sphere) reduceColorByZ(ci []float32, z float64) (r, g, b float32) {
+	rate := (float32(-z) + 1.0) / 2.0
+	r = 1.0 - ((1.0 - ci[0]) * rate)
+	g = 1.0 - ((1.0 - ci[1]) * rate)
+	b = 1.0 - ((1.0 - ci[2]) * rate)
+	return
 }
 
 func (s *Sphere) convertCoordinate(xi, yi float64) (xo, yo, zo float64) {
